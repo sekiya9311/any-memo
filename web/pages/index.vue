9273ad1696e9305ebf61f9a175b3bc25d8cf9e8b
sekiya9311 <sekiya9311@gmail.com>
2018-12-14 01:27:18 +0900

    ログイン処理っぽいの

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -2,17 +2,34 @@
   <v-layout>
     <v-flex xs4>
       <!-- login and input -->
+      <div v-if="!!user"><!-- already auth --></div>
+      <div v-else>
+        <v-btn
+          color="info"
+          @click="authGoogle">auth Google</v-btn>
+      </div>
     </v-flex>
     <v-flex xs8>
       <!-- memo list -->
+      <div v-if="!!user && !!userData"/>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
   data() {
     return {}
+  },
+  computed: {
+    ...mapGetters(['user', 'userData'])
+  },
+  async mounted() {
+    await this.$store.dispatch('getUser')
+  },
+  methods: {
+    ...mapActions(['authGoogle'])
   }
 }
 </script>
