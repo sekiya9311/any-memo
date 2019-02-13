9cce0f28426b3c049e4d8e8097fe163b58e004b3
sekiya9311 <sekiya9311@gmail.com>
2019-02-14 00:16:49 +0900

    Fix test

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:any_memo/main.dart';
+import 'package:any_memo/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
